8219eb373c7c636f28d4cf722211167866fb51ee
mels10224 <63226666+mels10224@users.noreply.github.com>
2024-07-05 17:18:40 +0300

    Delete TelegramRemoteControlBot directory

--- TelegramRemoteControlBot/config.py
+++
@@ -1,5 +0,0 @@
-
-# config.py
-TOKEN = '7470493849:AAH2_jREQoTHnjUe5sAQmA_B_TWLsePbAdc'
-YOUR_CHAT_ID = '845800521'
-FILE_IO_API_URL = "https://file.io"
--- TelegramRemoteControlBot/install_requirements.bat
+++
@@ -1,6 +0,0 @@
-
-@echo off
-echo Установка необходимых библиотек...
-pip install pyTelegramBotAPI pillow
-echo Установка завершена.
-pause
